a5975e498cb5c7f8832832fc82af34a30c1cb9c7
Jul Jen <juliajen.prog@gmail.com>
2018-03-23 21:11:21 +0000

    automatically backed up by learn

--- db/mkdir/01_create_artists.rb
+++ db/mkdir/01_create_artists.rb
@@ -22,12 +22,10 @@
 class CreateArtists < ActiveRecord::Migration
 
   def change
-    def change
-      create_table :artists do |t|
-        t.string :name
-        t.string :genre
-        t.integer :age
-        t.string :hometown
-      end
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
     end
   end
